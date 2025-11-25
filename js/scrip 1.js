9bcc9f2c831ef4bc12cd1074065c47eaf0933d89
leof118 <florescespedesleonardo@gmail.com>
2025-11-25 12:54:51 -0500

    s

--- js/scrip 1.js
+++ js/scrip 1.js
@@ -7,21 +7,21 @@ function triste(){
 }
 function feliz(){
     document.getElementById('carita').src='images/feliz.jpg';
-    alert("que habras hecho")
+    alert("que habras hecho");
 }
 function cambiarColor(){
     document.getElementById('men').style.color='#dac23aff';
-    alert("No te gusto el color :c")
+    alert("No te gusto el color :c");
 }
 function cambiarLogo(){
     document.getElementById('logo').src='images/logo 2.jpg';
-    alert("logo simple, te entiendo")
+    alert("logo simple, te entiendo");
 }
 function cambiarFondo(){
      document.getElementById('menu').style.background='#1a9c2bff';
-     alert("Nada te gusta :/")
+     alert("Nada te gusta :/");
 }
 function cambiodeTexto(){
     document.getElementById('Subtitulo').textContent="COMPLETADO";
-    alert("felicidades")
+    alert("felicidades");
 }
