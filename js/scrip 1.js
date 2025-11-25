168dd937c6570b4fbeeaabce70fec7c1dff82e35
leof118 <florescespedesleonardo@gmail.com>
2025-11-25 11:42:44 -0500

    sa

--- js/scrip 1.js
+++ js/scrip 1.js
@@ -2,18 +2,18 @@ console.log("Hola Mundo");
 alert("Hola Mundo, esto es un alert") 
 
 function triste(){
-    document.getElementById('carita').src='images/sad.png';
+    document.getElementById('carita').src='images/sad.jpg';
     alert("Po que ta triste mano");
 }
 function feliz(){
-    document.getElementById('carita').src='images/happy.png';
+    document.getElementById('carita').src='images/feliz.jpg';
 }
 function cambiarColor(){
     document.getElementById('menu').style.color='#eed33aff';
 
 }
 function cambiarLogo(){
-    document.getElementById('logo').src='images/logo2.jpg';
+    document.getElementById('logo').src='images/logo 2.jpg';
 }
 function cambiarFondo(){
      document.getElementById('menu').style.background='#524604';
